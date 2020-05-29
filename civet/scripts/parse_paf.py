128b0c00b4e7a2c26619bd911df052e25a53d164
aineniamh <aine.otoole@ed.ac.uk>
2020-05-29 14:00:36 +0100

    parse_paf script for taking top hit from minimap2 paf output

---
+++ civet/scripts/parse_paf.py
@@ -0,0 +1,80 @@
+import csv
+import argparse
+from Bio import SeqIO
+
+"""
+--paf {input.paf:q} 
+--metadata {input.metadata:q} 
+--csv-out {output.csv:q} 
+"""
+def parse_args():
+    parser = argparse.ArgumentParser(description='Parse barcode info and minimap paf file, create report.')
+
+    parser.add_argument("--paf", action="store", type=str, dest="paf_file")
+    parser.add_argument("--metadata", action="store", type=str, dest="metadata")
+    parser.add_argument("--csv-out", action="store", type=str, dest="outfile")
+
+    return parser.parse_args()
+
+
+def parse_line(line):
+    values = {}
+    
+    tokens = line.rstrip('\n').split('\t')
+    values["name"], values["read_len"] = tokens[:2]
+    values["query_start"] = tokens[2]
+    values["query_end"] = tokens[3]
+    values["ref_hit"], values["ref_len"], values["coord_start"], values["coord_end"], values["matches"], values["aln_block_len"] = tokens[5:11]
+
+    return values
+
+
+def get_closest_cog_sequences(paf):
+
+    closest_cog_sequences = []
+
+    with open(paf,"r") as f:
+        last_mapping = None
+        for line in f:
+
+            mapping = parse_line(line)
+
+            if last_mapping:
+                if mapping["name"] == last_mapping["name"]:
+                    last_mapping['ref_hit'] = '?'
+                else:
+                    closest_cog_sequences.append(last_mapping)
+                    last_mapping = mapping
+            else:
+                last_mapping = mapping
+
+        closest_cog_sequences.append(last_mapping)
+
+    return closest_cog_sequences
+
+
+def parse_paf_and_get_metadata():
+    args = parse_args()
+
+    closest_cog = get_closest_cog_sequences(args.paf)
+
+    with open(args.metadata, newline="") as f:
+        rows_to_write = []
+        reader = csv.DictReader(f)
+        header_names = reader.fieldnames
+        for row in reader:
+            if row["sequence_name"] in closest_cog:
+                rows_to_write.append(row)
+    
+        with open(args.outfile, "w") as fw:
+            writer = csv.DictWriter(fw, fieldnames=header_names)
+            writer.writeheader()
+            writer.writerows(rows_to_write)
+
+
+
+
+if __name__ == '__main__':
+
+    parse_paf_and_get_metadata()
+    
