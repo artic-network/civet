6fd8ca19674bfcadbc50ee4868720f174abd1b80
aineniamh <aine.otoole@ed.ac.uk>
2020-06-08 17:40:00 +0100

    more descriptive arguments

--- civet/scripts/make_report.py
+++ civet/scripts/make_report.py
@@ -6,52 +6,55 @@ import argparse
 
 thisdir = os.path.abspath(os.path.dirname(__file__))
 
-def make_report(big_metadata, input_metadata, name_stem, output_directory, input_directory, desired_fields):
-    fd = os.path.join(output_directory, "figures")
+def make_report(cog_metadata, input_csv, outfile, outdir, tree_dir, fields, report_template):
 
-    outfile = os.path.join(output_directory, f"{name_stem}.pmd")
+    fd = os.path.join(outdir, "figures")
+    name_stem = ".".join(outfile.split(".")[:-1])
+
     with open(outfile, 'w') as pmd_file:
     
-        pmd_string = name_stem + ".pmd"
-
-        md_template = os.path.join(thisdir,'utils','civet_template.pmd')
+        md_template = report_template
         
         with open(md_template) as f:
             for l in f:
                 if "##CHANGE" in l:
                     if "output_directory" in l:
-                        new_l = f'output_directory = "{output_directory}"\n'
+                        new_l = f'output_directory = "{outdir}"\n'
                     elif "name_stem" in l:
                         new_l = f'name_stem = "{name_stem}"\n'
                     elif "big_metadata" in l:
-                        new_l = f'big_metadata = "{big_metadata}"\n'
+                        new_l = f'big_metadata = "{cog_metadata}"\n'
                     elif "their_metadata" in l:
-                        new_l = f'their_metadata = "{input_metadata}"\n'
+                        new_l = f'their_metadata = "{input_csv}"\n'
                     elif "input_directory" in l:
-                        new_l = f'input_directory = "{input_directory}"\n'
+                        new_l = f'input_directory = "{tree_dir}"\n'
                     elif "desired_fields" in l:
-                        new_l = f'desired_fields = "{desired_fields}"\n'
+                        new_l = f'desired_fields = "{fields}"\n'
 
                 else:
                     new_l = l
 
                 pmd_file.write(new_l)
 
-    weave(pmd_string, doctype = "pandoc", figdir=fd)
+    weave(outfile, doctype = "pandoc", figdir=fd)
 
 def main():
     parser = argparse.ArgumentParser(description="Report generator script")
-    parser.add_argument("-i","--input-path", required=False, default="", help="path to tree directory",dest="input_path")
-    parser.add_argument("-bm","--cog-metadata", required=True, help="path to full COG metadata file",dest="cog_metadata")
-    parser.add_argument("-im","--input-metadata", required=False, help="path to their metadata file",dest="input_metadata")
-    parser.add_argument("-s","--stem", default="civet_report", help="output name stem as a string",dest="stem")
-    parser.add_argument("-od","--outdir", help="output directory",dest="outdir")
-    parser.add_argument("-df", "--desired-fields",default="", help="desired fields for report. Default=date and UK country",dest="desired_fields")
+    parser.add_argument("-i","--input-csv", required=False, help="path to input file",dest="input_csv")
+    parser.add_argument("-f", "--fields",default="", help="desired fields for report. Default=date and UK country",dest="fields")
 
+    parser.add_argument("--filtered-cog-metadata", required=False, help="path to combined metadata file",dest="filtered_cog_metadata")
+    parser.add_argument("--cog-metadata", required=True, help="path to full COG metadata file",dest="cog_metadata")
+    
+    parser.add_argument("-t","--tree-dir", required=False, default="", help="path to tree directory",dest="tree_dir")
+    parser.add_argument("--report-template", help="report template file",dest="report_template")
+
+    parser.add_argument("-o","--outfile", default="civet_report.pmd", help="output name stem as a string",dest="outfile")
+    parser.add_argument("--outdir", help="output directory",dest="outdir")
 
     args = parser.parse_args()
 
-    make_report(args.cog_metadata, args.input_metadata, args.stem, args.outdir, args.input_path, args.desired_fields)
+    make_report(args.cog_metadata, args.input_csv, args.outfile, args.outdir, args.tree_dir, args.fields, args.report_template)
 
 
 if __name__ == "__main__":
