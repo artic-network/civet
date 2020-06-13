a51855bb81a3403eb0ff6f3b30b76a7640d57db2
aineniamh <aine.otoole@ed.ac.uk>
2020-06-13 19:08:27 +0100

    bug fix and adding figdir as cmd line arg

--- civet/scripts/make_report.py
+++ civet/scripts/make_report.py
@@ -7,19 +7,9 @@ import shutil
 
 thisdir = os.path.abspath(os.path.dirname(__file__))
 
-def make_report(cog_metadata, input_csv, filtered_cog_metadata, outfile, outdir, tree_dir, fields, report_template, font_file):
+def make_report(cog_metadata, input_csv, filtered_cog_metadata, outfile, outdir, treedir, fd, fields, report_template, font_file):
 
-    fd = os.path.join(outdir, "figures")
     name_stem = ".".join(outfile.split(".")[:-1])
-
-    to_data_dir = font_file.rstrip("HelveticaNeue.ttf") #this will need making more robust
-    source = os.path.join(to_data_dir, "polytomies.png")
-    destination = fd
-    try:
-        shutil.move(source, destination) 
-    except shutil.Error:
-        pass 
-    
     with open(outfile, 'w') as pmd_file:
     
         md_template = report_template
@@ -38,13 +28,13 @@ def make_report(cog_metadata, input_csv, filtered_cog_metadata, outfile, outdir,
                     elif "input_csv" in l:
                         new_l = f'input_csv = "{input_csv}"\n'
                     elif "input_directory" in l:
-                        new_l = f'input_directory = "{tree_dir}"\n'
+                        new_l = f'input_directory = "{treedir}"\n'
                     elif "desired_fields" in l:
                         new_l = f'desired_fields = "{fields}"\n'
                     elif "font_file" in l:
                         new_l = f'font_file = "{font_file}"\n'
                     elif "tree_dir" in l:
-                        new_l = f'tree_dir = "{tree_dir}"\n'
+                        new_l = f'tree_dir = "{treedir}"\n'
                 else:
                     new_l = l
 
@@ -60,16 +50,17 @@ def main():
     parser.add_argument("--filtered-cog-metadata", required=False, help="path to combined metadata file",dest="filtered_cog_metadata")
     parser.add_argument("--cog-metadata", required=True, help="path to full COG metadata file",dest="cog_metadata")
     
-    parser.add_argument("-t","--tree-dir", required=False, default="", help="path to tree directory",dest="tree_dir")
+    parser.add_argument("-t","--treedir", required=False, default="", help="path to tree directory",dest="treedir")
     parser.add_argument("--report-template", help="report template file",dest="report_template")
     parser.add_argument("--font-file", help="custom font",dest="font_file")
-    
+
     parser.add_argument("-o","--outfile", default="civet_report.pmd", help="output name stem as a string",dest="outfile")
     parser.add_argument("--outdir", help="output directory",dest="outdir")
+    parser.add_argument("--figdir", help="output directory",dest="figdir")
 
     args = parser.parse_args()
 
-    make_report(args.cog_metadata, args.input_csv, args.filtered_cog_metadata, args.outfile, args.outdir, args.tree_dir, args.fields, args.report_template, args.font_file)
+    make_report(args.cog_metadata, args.input_csv, args.filtered_cog_metadata, args.outfile, args.outdir, args.treedir, args.figdir,args.fields, args.report_template, args.font_file)
 
 
 if __name__ == "__main__":
