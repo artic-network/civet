01a9a3dc4113bed78281b21ce3efbd34e67f0049
Stefan <S.Rooke93@gmail.com>
2020-07-02 13:38:18 +0100

    Integrating lineage code to report structure

--- civet/scripts/local_scale_analysis.py
+++ civet/scripts/local_scale_analysis.py
@@ -1,30 +1,40 @@
+if __name__ == "__main__":
+    main()
+
 import geopandas as gp
 import pandas as pd
 from libpysal.weights import Queen, attach_islands, DistanceBand, set_operations
 import datetime as dtime
 import json
 import pickle
+from vega import VegaLite
+from IPython.display import display,display_png
+from IPython.display import display_html
+def VegaLite(spec):
+    bundle = {}
+    bundle['application/vnd.vegalite.v3+json'] = spec
+    display(bundle, raw=True)
 
 def adm2cleaning(data_cog):
-    data_cog.dropna(subset=['adm2'],inplace=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^MOTHERWELL$','NORTH LANARKSHIRE', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^GREATER_LONDON$','GREATER LONDON', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^BORDERS$','SCOTTISH BORDERS', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^PAISLEY$','RENFREWSHIRE', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^SUTTON-IN-ASHFIELD$','NOTTINGHAMSHIRE', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^LONDON$','GREATER LONDON', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^KILMARNOCK$','EAST AYRSHIRE', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^PERTH$','PERTHSHIRE AND KINROSS', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^ISLE OF ANGLESEY$','ANGLESEY', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^SHETLAND$','SHETLAND ISLANDS', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^GREATER LONDONDERRY$','LONDONDERRY', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^STAFFORD$','STAFFORDSHIRE', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^INVERNESS$','HIGHLAND', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^KINGS NORTON$','WEST MIDLANDS', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^GLASGOW CITY$','GLASGOW', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^CITY OF LONDON$','GREATER LONDON', regex=True)
-    data_cog['adm2']=data_cog['adm2'].str.replace('^RHONDDA CYNON TAF$','RHONDDA, CYNON, TAFF', regex=True)
-    return data_cog
+    data_cog2=data_cog.dropna(subset=['adm2']).copy()
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^MOTHERWELL$','NORTH LANARKSHIRE', regex=True).copy()
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^GREATER_LONDON$','GREATER LONDON', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^BORDERS$','SCOTTISH BORDERS', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^PAISLEY$','RENFREWSHIRE', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^SUTTON-IN-ASHFIELD$','NOTTINGHAMSHIRE', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^LONDON$','GREATER LONDON', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^KILMARNOCK$','EAST AYRSHIRE', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^PERTH$','PERTHSHIRE AND KINROSS', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^ISLE OF ANGLESEY$','ANGLESEY', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^SHETLAND$','SHETLAND ISLANDS', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^GREATER LONDONDERRY$','LONDONDERRY', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^STAFFORD$','STAFFORDSHIRE', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^INVERNESS$','HIGHLAND', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^KINGS NORTON$','WEST MIDLANDS', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^GLASGOW CITY$','GLASGOW', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^CITY OF LONDON$','GREATER LONDON', regex=True)
+    data_cog2['adm2']=data_cog['adm2'].str.replace('^RHONDDA CYNON TAF$','RHONDDA, CYNON, TAFF', regex=True)
+    return data_cog2
 
 
 def dateRestriction(DFin, start_date, end_date):
@@ -76,9 +86,8 @@ def getForthBridge(DFin, weightedMatrix):
     w_fixed_setUnion=set_operations.w_union(weightedMatrix,landbridge)
     return w_fixed_setUnion
 
-def mappie():
-    multimappie_blank = """{
-    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+singlemappie_blank = '''{
+  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
     "width": 1000,
     "height": 1500,
     "projection": {"type":"mercator"},
@@ -101,123 +110,132 @@ def mappie():
     ],
     "resolve": {"scale":{"color":"independent", "radius":"independent", "scale":"independent", "theta":"independent"} }
     }
-    """
-    return multimappie_blank
+    '''
 
-def multimappie():
-    multimappie_blank = """{
-    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-    "width": 1000,
-    "height": 1500,
-    "projection": {"type":"mercator"},
-    "layer": [
-      {
-          "data": {
-          "values":""
-          },
-
-        "mark": {"type":"geoshape", "fill": "#112B31", "stroke": "white"}
-    },
-      {
-          "data": {
-          "values":""
-          },
-
-        "mark": {"type":"geoshape", "fill": "#D6D6D6", "stroke": "white"}
-    }
-
-    ],
-    "resolve": {"scale":{"color":"independent", "radius":"independent", "scale":"independent", "theta":"independent"} }
-    }
-    """
-    return multimappie_blank
-
-def blankJSON():
-    blank_json = """{
+multimappie_blank="""{
+"$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+"width": 1000,
+"height": 1500,
+"projection": {"type":"mercator"},
+"layer": [
+  {
       "data": {
-        "values": ""
+      "values":""
       },
 
-      "layer": [
-        {
-        "mark": {"type": "arc", "innerRadius": 20, "cornerRadius":0.5,"padAngle":0.05, "stroke": "white","strokeWidth":0.5, "thetaOffset":-1.5},
-        "encoding": { 
-        "text": {"field": "UK_Lineage", "type": "nominal"}
-            }
-        },
-            {
-        "mark": {"type": "text", "radiusOffset": 30, "radius":30, "fill":"black", "fontWeight":"bold", "fontSize":15, "font":"Helvetica Neue","angle":0,"align":"center", "thetaOffset":-1.5, "stroke":"white", "strokeWidth":0.1},
-        "encoding": { 
-          "text": {"field": "UK_Lineage", "type": "nominal"}
-            }
-        }
-    ],
-      "encoding": {
-        "theta": {"field": "Count", "type": "quantitative", "sort":"descending", "stack": true},
-        "radius": {"field": "Count", "type": "quantitative", "scale": {"type": "sqrt", "zero": true, "range": [10, 100]}},
-        "color": {"field": "UK_Lineage", "sort":"ascending", "type": "nominal", "scale":{"range": ["#344966", "#E0BE15", "#7B517B", "#5374A2", "#5B8E7D", "#B34248", "#023C40", "#0D1821", "#69626D","#C2C2C2"]}},
-          "latitude" : {"datum":""},
-          "longitude":{"datum":""}
-            }
-    }"""
-    return blank_json
+    "mark": {"type":"geoshape", "fill": "#D6D6D6", "stroke": "white"}
+},
+  {
+      "data": {
+      "values":""
+      },
 
-def tabulateLins(HBCODE, DF):
-    for each in HBCODE:
+    "mark": {"type":"geoshape", "fill": "#112B31", "stroke": "white"}
+}
+],
+"resolve": {"scale":{"color":"independent", "radius":"independent", "scale":"independent", "theta":"independent"} }
+}
+"""
+
+
+blank_json="""{
+  "data": {
+    "values": ""
+  },
+
+  "layer": [
+    {
+    "mark": {"type": "arc", "innerRadius": 20, "cornerRadius":0.5,"padAngle":0.05, "stroke": "white","strokeWidth":0.5, "thetaOffset":-1.5},
+    "encoding": { 
+    "text": {"field": "UK_Lineage", "type": "nominal"}
+        }
+    },
+        {
+    "mark": {"type": "text", "radiusOffset": 30, "radius":30, "fill":"black", "fontWeight":"bold", "fontSize":15, "font":"Helvetica Neue","angle":0,"align":"center", "thetaOffset":-1.5, "stroke":"white", "strokeWidth":0.1},
+    "encoding": { 
+      "text": {"field": "UK_Lineage", "type": "nominal"}
+        }
+    }
+],
+  "encoding": {
+    "theta": {"field": "Count", "type": "quantitative", "sort":"descending", "stack": true},
+    "radius": {"field": "Count", "type": "quantitative", "scale": {"type": "sqrt", "zero": true, "range": [10, 100]}},
+    "color": {"field": "UK_Lineage", "sort":"ascending", "type": "nominal", "scale":{"range": ["#344966", "#E0BE15", "#7B517B", "#5374A2", "#5B8E7D", "#B34248", "#023C40", "#0D1821", "#69626D","#C2C2C2"]}},
+      "latitude" : {"datum":""},
+      "longitude":{"datum":""}
+        }
+}"""
+
+def tabulateLins(HBCODE, DF, HBNAME):
+    #lineage_uk=cog_mainland_daterestricted.loc[cog_mainland_daterestricted['HBCode'] == HBCODE]['uk_lineage'].value_counts()[:10].to_frame()
+    for each in [HBCODE]:
         if len(DF.loc[DF['HBCode']==each]['uk_lineage'].value_counts().to_frame()) > 20:
-            uk_lin_DF=DF.loc[DF['HBCode'] == each]['uk_lineage'].value_counts[:20].to_frame()
+            uk_lin_DF=DF.loc[DF['HBCode'] == each]['uk_lineage'].value_counts()[:20].to_frame()
             uk_lin_DF=uk_lin_DF.reset_index()
             uk_lin_DF.columns=['UK Lineage', 'Count']
         else:
-            uk_lin_DF=DF.loc[DF['HBCode'] == each]['uk_lineage'].value_counts.to_frame()
+            uk_lin_DF=DF.loc[DF['HBCode'] == each]['uk_lineage'].value_counts().to_frame()
             uk_lin_DF=uk_lin_DF.reset_index()
             uk_lin_DF.columns=['UK Lineage', 'Count']
         if len(DF.loc[DF['HBCode']==each]['lineage'].value_counts().to_frame()) > 10:
-            glob_lin_DF=DF.loc[DF['HBCode'] == each]['lineage'].value_counts[:10].to_frame()
-            glob_lin_DF=uk_lin_DF.reset_index()
+            glob_lin_DF=DF.loc[DF['HBCode'] == each]['lineage'].value_counts()[:10].to_frame()
+            glob_lin_DF=glob_lin_DF.reset_index()
             glob_lin_DF.columns=['Global Lineage', 'Count']
         else:
-            glob_lin_DF=DF.loc[DF['HBCode'] == each]['lineage'].value_counts.to_frame()
-            glob_lin_DF=uk_lin_DF.reset_index()
+            glob_lin_DF=DF.loc[DF['HBCode'] == each]['lineage'].value_counts().to_frame()
+            glob_lin_DF=glob_lin_DF.reset_index()
             glob_lin_DF.columns=['Global Lineage', 'Count']
     tableList=[uk_lin_DF, glob_lin_DF]
+    print(HBNAME)
+    print(uk_lin_DF.to_markdown())
+    print(glob_lin_DF.to_markdown())
+
     return tableList
 
 
 def mapProduce(HBSet, DF, region):
-    mapPIE=json.loads(mappie())
-    multimapPIE=json.loads(multimappie())
+    mapPIE=json.loads(multimappie_blank)
+    #multimapPIE=json.loads(multimappie())
+    HBSet_json = HBSet.to_json()
+    HBSet_json = json.loads(HBSet_json)
+    region_json = region.to_json()
+    region_json = json.loads(region_json)
+    #print(HBSet.features)
     if len(HBSet) == 1:
-        code=HBSet['HBCode'][0]
-        lat=HBSet.iloc[0].centroid.y.item()
-        long=HBSet.iloc[0].centroid.x.item()
-        tempJSON=blankJSON()
-        tempJSON=json.loads(tempJSON)
-        data=uk_lineage_json(code, DF)
-        tempJSON['data']['values']=data
-        tempJSON['encoding']['longitude']['datum']=long
-        tempJSON['encoding']['latitude']['datum']=lat
-        mapPIE['layer'].append(tempJSON)
-        mapPIE['layer'][0]['data']['values']=region
-        mapPIE['layer'][1]['data']['values']=HBSet['features']
+        for row, frame in HBSet.iterrows():
+            code=frame['HBCode']
+            lat=HBSet.loc[HBSet.index == row].centroid.y.item()
+            long=HBSet.loc[HBSet.index == row].centroid.x.item()
+            tempJSON=json.loads(blank_json)
+            data=uk_lineage_json(code, DF)
+            #print(data)
+            tempJSON['data']['values']=data
+            tempJSON['encoding']['longitude']['datum']=long
+            tempJSON['encoding']['latitude']['datum']=lat
+            mapPIE['layer'].append(tempJSON)
+        mapPIE['layer'][0]['data']['values']=region_json['features']
+        mapPIE['layer'][1]['data']['values']=HBSet_json['features']
+        VegaLite(mapPIE)
+        return mapPIE
     elif len(HBSet) > 1:
         for row, frame in HBSet.iterrows():
             code=frame['HBCode']
             lat = HBSet.loc[HBSet.index == row].centroid.y.item()
             long = HBSet.loc[HBSet.index == row].centroid.x.item()
-            tempjson = blankJSON()
-            tempjson = json.loads(tempjson)
-            data = uk_lineage_json(code, DF)
-            tempjson['data']['values'] = data
-            tempjson['encoding']['longitude']['datum'] = long
-            tempjson['encoding']['latitude']['datum'] = lat
-            multimapPIE['layer'].append(tempjson)
-        mapPIE['layer'][0]['data']['values'] = HBSet['features']
-        mapPIE['layer'][1]['data']['values'] = region
+            tempJSON=json.loads(blank_json)
+            data=uk_lineage_json(code, DF)
+            tempJSON['data']['values']=data
+            tempJSON['encoding']['longitude']['datum']=long
+            tempJSON['encoding']['latitude']['datum']=lat
+            mapPIE['layer'].append(tempJSON)
+        mapPIE['layer'][0]['data']['values'] = HBSet_json['features']
+        mapPIE['layer'][1]['data']['values'] = region_json['features']
+        return mapPIE
 
 def getSampleData(metadataLoc):
     cog_meta = pd.read_csv(metadataLoc)
-    cog_meta['sample_date'] = pd.to_datetime(cog_meta_mainland['sample_date'], format="%Y-%m-%d")
+    cog_meta['sample_date'] = pd.to_datetime(cog_meta['sample_date'], format="%Y-%m-%d")
+    return cog_meta
 
 
 def getSampleData_final(MetadataDF, dates=None):
@@ -237,14 +255,18 @@ def central_surrounding_regions(HBCODE, neighborW, MAP):
     neighbors=subregion.loc[subregion['HBCode'] != HBCODE]
     return central, neighbors, subregion
 
-def adm2_to_centralHBCode(adm2, translation_dict):
+def adm2_to_centralHBCode(adm2, translation_dict, HbtoCode):
     HBs=[]
+    #print(adm2)
     for each in adm2:
         if each in translation_dict:
-          HBs.append(translation_dict[each])
+            HBs.append(translation_dict[each])
     if len(HBs) > 0:
         centralHB=max(HBs,key=HBs.count)
-        return centralHB
+        centralHBCode=HbtoCode[centralHB]
+        #[k for k, v in a.items() if v == b]
+        print(centralHBCode)
+        return centralHBCode
     elif len(HBs) == 0:
         return None
 
@@ -256,6 +278,9 @@ def defineDateRestriction(samplesDF, specifiedRange):
     if  len(datedSamples) == 1:
         daterange=''
         return daterange
+
+
+
 
 
 
@@ -295,6 +320,3 @@ def defineDateRestriction(samplesDF, specifiedRange):
 #    for each, region in [central, neighboring], ['Central Health Board', 'Neighboring Health Boards']:
 #        print (region)
 #        mapProduce(each, cog_meta_mainland, submap)
-
-if __name__ == "__main__":
-    main()
