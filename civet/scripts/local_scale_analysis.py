b29564bbe62666e270cb08f77707f754b389ac84
ViralVerity <verity.hill@ed.ac.uk>
2020-09-21 13:56:11 +0100

    adding local lineage analysis to config

--- civet/scripts/local_scale_analysis.py
+++ civet/scripts/local_scale_analysis.py
@@ -6,6 +6,7 @@ import geopandas as gp
 import pandas as pd
 from libpysal.weights import Queen, attach_islands, DistanceBand, set_operations
 import json
+import csv
 #from vega import VegaLite
 import argparse
 warnings.filterwarnings("ignore")
@@ -13,9 +14,9 @@ print("running local scale analysis")
 # ~~~~~~~ Define input variables ~~~~~~~~~~
 
 parser = argparse.ArgumentParser(description='Parse barcode info and minimap paf file, create report.')
-parser.add_argument("--date-restriction", default="False", action="store", type=str, dest="date_restriction")
-parser.add_argument("--date-pair-start", default="", action="store", type=str, dest="date_pair_start")
-parser.add_argument("--date-pair-end", default="", action="store", type=str, dest="date_pair_end")
+parser.add_argument("--date-restriction", default=False, action="store", type=bool, dest="date_restriction")
+parser.add_argument("--date-pair-start", default="", action="store", dest="date_pair_start")
+parser.add_argument("--date-pair-end", default="", action="store", dest="date_pair_end")
 parser.add_argument("--cog-meta-global", action="store", type=str, dest="cog_metadata")
 parser.add_argument("--user-sample-data", action="store", type=str, dest="user_sample_data")
 parser.add_argument("--date-window", action="store",required=False, type=int, dest="date_window")
@@ -43,7 +44,7 @@ mapfile=argsIN.uk_map
 
 date_pair=[]
 for each in [argsIN.date_pair_start, argsIN.date_pair_end]:
-    if each != "None":
+    if each != "False":
         date_pair.append(each)
 
 
@@ -56,10 +57,11 @@ for each in [argsIN.date_pair_start, argsIN.date_pair_end]:
 
 def adm2cleaning(data_cog, samplecsv=False):
     data_cog2 = ""
-    if samplecsv == False:
+    if not samplecsv:
         data_cog2 = data_cog.dropna(subset=['adm2']).copy()
-    if samplecsv == True:
+    else:
         data_cog2 = data_cog.copy()
+    
     data_cog2['adm2'] = data_cog2['adm2'].str.replace('^MOTHERWELL$', 'NORTH LANARKSHIRE', regex=True).copy()
     data_cog2['adm2'] = data_cog2['adm2'].str.replace('^GREATER_LONDON$', 'GREATER LONDON', regex=True)
     data_cog2['adm2'] = data_cog2['adm2'].str.replace('^BORDERS$', 'SCOTTISH BORDERS', regex=True)
@@ -343,19 +345,41 @@ def adm2_to_centralHBCode(sampleframe, translation_dict, HbtoCode):
     elif len(HBs) == 0:
         return None
 
-
 def defineDateRestriction(samplesDF, windowSize):
-    datedSamples = samplesDF.dropna(subset=['Collection_Date'])
-    if len(datedSamples) == 0:
-        print('No collection dates specified, will revert to using all available data for local lineage analysis.')
-        return None
-    if len(datedSamples) > 0:
-        datedSamples['Collection_Date'] = pd.to_datetime(datedSamples['Collection_Date'], format="%Y-%m-%d")
-        datedSamples.sort_values(by=['Collection_Date'], inplace=True)
-        start = datedSamples.iloc[[0]]['Collection_Date'].item() + pd.DateOffset(days=-windowSize)
-        end = datedSamples.iloc[[-1]]['Collection_Date'].item() + pd.DateOffset(days=windowSize)
-        daterange = {'start_date': start, 'end_date': end}
-        return daterange
+  
+  col_list = samplesDF.columns
+  potential_date_cols = []
+  for i in col_list:
+    if "date" in i.lower():
+      potential_date_cols.append(i)
+
+  if len(potential_date_cols) == 0:
+    print('No dates found, will revert to using all available data for local lineage analysis.')
+    return None
+
+  collection = False
+  for j in potential_date_cols:
+    if "collection" in j.lower():
+      date_col = j
+      collection = True
+      break
+    else:
+      date_col = j
+      
+  if collection:
+    print(f"Collection date not found, using {j} to restrict local lineages by")
+
+  datedSamples = samplesDF.dropna(subset=[date_col])
+  if len(datedSamples) == 0:
+      print('No collection dates specified, will revert to using all available data for local lineage analysis.')
+      return None
+  if len(datedSamples) > 0:
+      datedSamples[date_col] = pd.to_datetime(datedSamples[date_col], format="%Y-%m-%d")
+      datedSamples.sort_values(by=[date_col], inplace=True)
+      start = datedSamples.iloc[[0]][date_col].item() + pd.DateOffset(days=-windowSize)
+      end = datedSamples.iloc[[-1]][date_col].item() + pd.DateOffset(days=windowSize)
+      daterange = {'start_date': start, 'end_date': end}
+      return daterange
 
 
 def finaliseMapping(boardMAP):
@@ -371,15 +395,14 @@ def hbcode_hbname_translation(mapDF):
     translation = dict(zip(mapDF.HBName, mapDF.HBCode))
     return translation
 
-
-def do_date_restriction(cogDF, samplecsv, start, end, window=7, restriction_bool="False"):
-    if restriction_bool == 'True':
-        if start is not None and end is not None:
+def do_date_restriction(cogDF, samplecsv, start, end, window=7, restriction_bool=False):
+    if restriction_bool:
+        if start and end:
             date_list = {'start_date': date_start, 'end_date': date_end}
             cogOut = dateRestriction(cogDF, date_list)
             return cogOut
         else:
-            date_list = defineDateRestriction(inputSamples, window)
+            date_list = defineDateRestriction(samplecsv, window)
             if date_list is not None:
                 cogOut = dateRestriction(cogDF, date_list)
                 return cogOut
@@ -580,13 +603,10 @@ HBname_code_translation=hbname_hbcode_translation(mainland_boards)
 # ~~~~~~~
 #mainland_boards=update_adm15(mainland_boards)
 ###Checking user defined dates###
-if argsIN.date_restriction != 'True':
+if not argsIN.date_restriction: #!= "True"
     cog_meta=do_date_restriction(COGDATA, inputSamples, date_start, date_end)
-if argsIN.date_restriction == 'True':
-    if date_window == '':
-        cog_meta=do_date_restriction(COGDATA, inputSamples, date_start, date_end, restriction_bool='True')
-    if date_window != '':
-        cog_meta=do_date_restriction(COGDATA, inputSamples, date_start, date_end, restriction_bool='True', window=int(date_window))
+else:
+    cog_meta=do_date_restriction(COGDATA, inputSamples, date_start, date_end, restriction_bool=True, window=int(argsIN.date_window))
 ### Final restricted meta
 cog_final = getSampleData_final(cog_meta, HBTranslation, HBCode_name_translation)
 ## Proessing input csv ##
