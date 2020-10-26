dfd2b76f7a4c62dd94f69af0b4490e5c85f6d9a6
aineniamh <aine.otoole@ed.ac.uk>
2020-10-26 11:16:54 +0000

    adm2 check commented out

--- civet/command.py
+++ civet/command.py
@@ -273,8 +273,8 @@ def main(sysargs = sys.argv[1:]):
     # local lineages qc
     cfunk.local_lineages_qc(config)
 
-    # check adm2 values
-    cfunk.check_adm2_values(config)
+    # # check adm2 values
+    # cfunk.check_adm2_values(config)
 
     """
     Parsing the tree_group arguments, 
