920ebe4d68cf3dfc5d03acb06fa267c3ce3d9fec
aineniamh <aine.otoole@ed.ac.uk>
2021-04-09 15:01:55 +0100

    updating test to have fake fasta

--- civet/input_parsing/input_query_parsing.py
+++ civet/input_parsing/input_query_parsing.py
@@ -15,20 +15,3 @@ def make_csv_string_from_ids(ids, config):
             fw.write(i+'\n')
         print(green(f"Number of IDs:") + f" {c}")
     return query
-
-def input_csv_qc(cwd,inputfile):
-    input_file = os.path.join(cwd,inputfile)
-
-    ending = input_file.split(".")[-1]
-
-    if ending == "csv":
-        print(colour.green(f"Input file:") + f" {input_file}")
-        query = input_file
-
-    elif ending == "xls":
-        sys.stderr.write(colour.cyan(f"Error: it looks like you've provided an excel file as input. Please provide a csv file.\n"))
-        sys.exit(-1)
-    else:
-        sys.stderr.write(colour.cyan(f"Error: -i,--input accepts a csv file, please provide this or a comma-separated string of IDs.\n"))
-        sys.exit(-1)
-
