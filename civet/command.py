e01939e126c5a505c6616b27ef8581a704870405
aineniamh <aine.otoole@ed.ac.uk>
2021-05-18 15:34:15 +0100

    arg parsing of input data, now takes background snps optionally and background tree optionally

--- civet/command.py
+++ civet/command.py
@@ -5,6 +5,9 @@ from civet.input_parsing import initialising as init
 from civet.input_parsing import input_arg_parsing
 from civet.input_parsing import data_arg_parsing
 
+from civet.input_parsing import input_data_parsing
+
+
 from civet.utils import misc
 from civet.utils import dependency_checks
 from civet.utils import data_install_checks
@@ -34,29 +37,31 @@ def main(sysargs = sys.argv[1:]):
 
     i_group.add_argument('-f','--fasta', action="store",help="Optional fasta file. Sequence IDs must match to a query ID specified either in the input csv or ID string.", dest="fasta")
     i_group.add_argument('-n','--max-ambiguity', action="store", type=float,help="Maximum proportion of Ns allowed to attempt analysis. Default: 0.5",dest="max_ambiguity")
-    i_group.add_argument('-l','--min-length', action="store", type=int,help="Minimum query length allowed to attempt analysis. Default: 10000",dest="min_length")
+    i_group.add_argument('-l','--min-length', action="store", type=int,help="Minimum query length allowed to attempt analysis. Default: 20000",dest="min_length")
     
     i_group.add_argument('-fm','--from-metadata',nargs='*', dest="from_metadata",help="Generate a query from the metadata file supplied. Define a search that will be used to pull out sequences of interest from the large phylogeny. E.g. -fm adm2=Edinburgh sample_date=2020-03-01:2020-04-01")
 
     d_group = parser.add_argument_group('Background data options')
     d_group.add_argument('-d','--datadir', action="store",help="Directory containing the background data files.")
-    d_group.add_argument("-bc","--background-csv",action="store",dest="background_csv",help="Custom metadata file that corresponds to the large global tree/ alignment. Should have a column `sequence_name`.")
-    d_group.add_argument("-bf","--background-fasta", action="store", dest="background_fasta", help="Custom background fasta file.")
-    d_group.add_argument("-dcol",'--data-column', action="store",help="Column in background csv to match with input IDs. Default: sequence_name", dest="data_column")
+    d_group.add_argument("-bc","--background-csv",action="store",dest="background_csv",help="Custom metadata file for all background data. Should have a column matching '--data-column', Default: sequence_name.")
+    d_group.add_argument("-bSNP","--background-SNPs",action="store",dest="background_SNPs",help="Optional SNP file for all background data. Civet will calculate this file if not supplied, which may take some time. Should have a column matching '--data-column', Default: sequence_name.")
+    d_group.add_argument("-bf","--background-fasta", action="store", dest="background_fasta", help="Custom background fasta file for all background data. Sequence IDs should match the background metadata data_column.")
+    d_group.add_argument("-bt","--background-tree", action="store", dest="background_tree", help="Custom background tree file for all background data. Tip names should match the background metadata data_column.")
+    d_group.add_argument("-dcol",'--data-column', action="store",help="Column in background data to match with input IDs. Default: sequence_name", dest="data_column")
 
     o_group = parser.add_argument_group('Output options')
-    o_group.add_argument('-o','--output-prefix',action="store",help="Prefix of output directory & report name: Default: civet",dest="output_prefix")
     o_group.add_argument('--outdir', action="store",help="Output directory. Default: current working directory")
-    
+    o_group.add_argument('-o','--output-prefix',action="store",help="Prefix of output directory & report name: Default: civet",dest="output_prefix")
+
     o_group.add_argument('--output-data',action="store_true",help="Output intermediate data files to the output directory",dest="output_data")
     
-    o_group.add_argument("--no-temp",action="store_true",help="Output all intermediate files. For development/ debugging purposes.",dest="no_temp")
     o_group.add_argument('--tempdir',action="store",help="Specify where you want the temp stuff to go. Default: $TMPDIR")
+    o_group.add_argument("--no-temp",action="store_true",help="Output all intermediate files. For development/ debugging purposes.",dest="no_temp")
 
-    
     misc_group = parser.add_argument_group('misc options')
     misc_group.add_argument("--safety-level", action="store", type=int, dest="safety_level",help="Level of anonymisation for users. Options: 0 (no anonymity), 1 (no COGIDs on background data), 2 (no adm2 on data). Default: 1")
     misc_group.add_argument("--verbose",action="store_true",help="Print lots of stuff to screen")
+    misc_group.add_argument("--debug",action="store_true",help="Debugging mode.")
     misc_group.add_argument("--art",action="store_true",help="Print art")
     misc_group.add_argument('-t', '--threads', action='store',dest="threads",type=int,help="Number of threads")
     misc_group.add_argument("-v","--version", action='version', version=f"civet {__version__}")
@@ -84,8 +89,9 @@ def main(sysargs = sys.argv[1:]):
     input_arg_parsing.input_fasta_parsing(args.fasta,args.max_ambiguity,args.min_length,config)
 
     data_install_checks.check_install(config)
-    data_arg_parsing.data_group_parsing(args.datadir,args.background_csv,args.background_fasta,args.data_column,config)
+    data_arg_parsing.data_group_parsing(args.debug,args.datadir,args.background_csv,args.background_SNPs,args.background_fasta,args.background_tree,args.data_column,config)
     
+    query_metadata, passed_qc_fasta = input_data_parsing.query_check_against_background_merge_input(config)
 
     for i in sorted(config):
         print(i, config[i])
--- civet/input_parsing/data_arg_parsing.py
+++ civet/input_parsing/data_arg_parsing.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from civet.utils import log_colours as colour
+from civet.utils.log_colours import green,cyan
 from civet.utils import misc
 import sys
 import os
@@ -8,19 +8,21 @@ import pkg_resources
 from collections import Counter
 from Bio import SeqIO
 
-def check_dir_for_file(file_list, extension,argument,key,config):
+def check_dir_for_file(file_list, extension,argument,key,config,required):
     match = []
     for fn in file_list:
         if fn.endswith(extension):
             match.append(fn)
     if len(match) > 1:
-        sys.stderr.write(colour.cyan(f"Error: More than 1 {extension} file found in {config['datadir']},\nplease specify which {extension} file using {argument}.\n"))
+        sys.stderr.write(cyan(f"Error: More than 1 {extension} file found in {config['datadir']},\nplease specify which {extension} file using {argument}.\n"))
         sys.exit(-1)
     elif len(match) == 0:
-        sys.stderr.write(colour.cyan(f"Error: No {extension} file found in {config['datadir']},\nplease specify which {extension} file using {argument}.\n"))
-        sys.exit(-1)
+        if required==True:
+            sys.stderr.write(cyan(f"Error: No {extension} file found in {config['datadir']},\nplease specify which {extension} file using {argument}.\n"))
+            sys.exit(-1)
     elif len(match) == 1:
         config[key] = match[0]
+
 
 def background_data_load(config):
     file_list = []
@@ -28,47 +30,53 @@ def background_data_load(config):
         for fn in f:
             file_list.append(os.path.join(r,fn))
     if not config["background_csv"]:
-        check_dir_for_file(file_list, "csv","-bc/--background-csv","background_csv",config)
+        check_dir_for_file(file_list, "metadata.csv","-bc/--background-csv","background_csv",config,True)
     if not config["background_fasta"]:
-        check_dir_for_file(file_list, "fasta","-bf/--background-fasta","background_fasta",config)
+        check_dir_for_file(file_list, "fasta","-bf/--background-fasta","background_fasta",config,True)
+
+    if not config["background_SNPs"]:
+        check_dir_for_file(file_list, "mutations.csv","-bSNP/--background-SNPs","background_SNPs",config,False)
+
+    if not config["background_tree"]:
+        check_dir_for_file(file_list, "newick","-bt/--background-tree","background_tree",config,False)
 
 def check_datadir(config):
     datadir = config["datadir"]
     if not os.path.exists(datadir):
         if not config["background_csv"] or not config["background_fasta"]:
-            print(colour.cyan(f"Error: data directory not found: {datadir}.\n")+"Please check the datadir path (-d/ --datadir) provided exists or supply files with -bc/ --background-csv and -bf/ --background-fasta.\n")
+            print(cyan(f"Error: data directory not found: {datadir}.\n")+"Please check the datadir path (-d/ --datadir) provided exists or supply files with -bc/ --background-csv and -bf/ --background-fasta.\n")
             sys.exit(-1)
 
-def check_background_csv(background_csv,data_column):
+def check_csv_file(argument,description,csv_file,data_column):
     
-    ending = background_csv.split(".")[-1]
+    ending = csv_file.split(".")[-1]
 
     if ending in ["xls","xlsx"]:
-        sys.stderr.write(colour.cyan(f"Error: it looks like you've provided an excel file as input.\n-bc,--background-csv accepts a csv file.\n"))
+        sys.stderr.write(cyan(f"Error: it looks like you've provided an excel file as input.\n{argument} accepts a csv file.\n"))
         sys.exit(-1)
     elif ending == "csv":
         pass
     else:
-        sys.stderr.write(colour.cyan(f"Error: please provide a csv file for -bc/ --background-csv.\n"))
+        sys.stderr.write(cyan(f"Error: please provide a csv file for {argument}.\n"))
         sys.exit(-1)
     
-    if os.path.isfile(background_csv):
+    if os.path.isfile(csv_file):
         try:
             c = 0
-            with open(background_csv,"r") as f:
+            with open(csv_file,"r") as f:
                 reader = csv.DictReader(f)
                 for row in reader:
                     c +=1
-            print(colour.green(f"{c} rows in background csv file."))
+            print(green(f"{c} rows in {description} file."))
         except:
-            sys.stderr.write(colour.cyan(f"Unable to read background csv file, please check your background csv is in the correct format with a header.\n"))
+            sys.stderr.write(cyan(f"Unable to read {description} file, please check your {description} is in the correct format with a header.\n"))
             sys.exit(-1)
     else:
-        sys.stderr.write(colour.cyan(f"Cannot find csv file: ")+f"{background_csv}\n")
+        sys.stderr.write(cyan(f"Cannot find {description} file: ")+f"{csv_file}\n")
         sys.exit(-1)
-    print(colour.green(f"Background csv file:") + f" {background_csv}")
+    print(green(f"{description.replace('b','B')} file:") + f" {csv_file}")
 
-    with open(background_csv,"r") as f:
+    with open(csv_file,"r") as f:
         reader = csv.DictReader(f)
         if data_column in reader.fieldnames:
             pass
@@ -76,10 +84,10 @@ def check_background_csv(background_csv,data_column):
             encode = False
             for i in reader.fieldnames:
                 if "ufeff" in i:
-                    sys.stderr.write(colour.cyan(f"Error: it appears your csv file may have been edited in Excel and now contains hidden characters.\n") + "Please remove said characters in a text editor and try again.")
+                    sys.stderr.write(cyan(f"Error: it appears your {description} file may have been edited in Excel and now contains hidden characters.\n") + "Please remove said characters in a text editor and try again.")
                     sys.exit(-1)
             else:
-                sys.stderr.write(colour.cyan(f"Error: {data_column} column not found in background csv file. Please specifiy which column to match with -dcol/ --data-column.\n"))
+                sys.stderr.write(cyan(f"Error: {data_column} column not found in {description} file. Please specifiy which column to match with `-dcol/ --data-column.`\n"))
                 sys.exit(-1)
     return c
 
@@ -87,10 +95,10 @@ def check_background_fasta(background_fasta):
     ending = background_fasta.split(".")[-1]
 
     if ending not in ["fa","fasta","fas"]:
-        sys.stderr.write(colour.cyan(f"Please input background sequences in fasta format, with file extension reflecting that.\n"))
+        sys.stderr.write(cyan(f"Please input background sequences in fasta format, with file extension reflecting that.\n"))
         sys.exit(-1)
     else:
-        print(colour.green(f"Background fasta file:") + f" {background_fasta}")
+        print(green(f"Background fasta file:") + f" {background_fasta}")
     lens = Counter()
     c = 0
     for record in SeqIO.parse(background_fasta, "fasta"):
@@ -98,10 +106,10 @@ def check_background_fasta(background_fasta):
         c +=1
 
     if c == 0:
-        sys.stderr.write(colour.cyan(f"Error: no records found in fasta file, please check your background fasta is in the correct format.\n"))
+        sys.stderr.write(cyan(f"Error: no records found in fasta file, please check your background fasta is in the correct format.\n"))
         sys.exit(-1)
     else:
-        print(colour.green(f"{c} records in background fasta file."))
+        print(green(f"{c} records in background fasta file."))
     
     if len(lens)>1:
         len_string = ""
@@ -110,11 +118,11 @@ def check_background_fasta(background_fasta):
                 len_string += f"\t- {lens[i]} record is {i} bases\n"
             else:
                 len_string += f"\t- {lens[i]} records are {i} bases\n"
-        sys.stderr.write(colour.cyan(f"Error: not all records are the same length, please check your background fasta is aligned correctly.\n") + len_string)
+        sys.stderr.write(cyan(f"Error: not all records are the same length, please check your background fasta is aligned correctly.\n") + len_string)
         sys.exit(-1)
     return c
 
-def data_group_parsing(datadir,background_csv,background_fasta,data_column,config):
+def data_group_parsing(debug,datadir,background_csv,background_SNPs,background_fasta,background_tree,data_column,config):
     """
     parses the data group arguments 
     --datadir (Default $DATADIR)
@@ -128,13 +136,15 @@ def data_group_parsing(datadir,background_csv,background_fasta,data_column,confi
     # if command line arg, overwrite config value
     misc.add_arg_to_config("datadir",datadir,config)
     misc.add_file_to_config("background_csv",background_csv,config)
+    misc.add_file_to_config("background_SNPs",background_SNPs,config)
     misc.add_file_to_config("background_fasta",background_fasta,config)
+    misc.add_file_to_config("background_tree",background_tree,config)
     misc.add_arg_to_config("data_column",data_column,config)
 
     # needs either datadir specified or both the files specified
     if not config["datadir"]:
         if not config["background_csv"] and not config["background_fasta"]:
-            sys.stderr.write(colour.cyan(f"Error: insufficient background data supplied, please provide a background csv and fasta file.\n"))
+            sys.stderr.write(cyan(f"Error: insufficient background data supplied, please provide a background csv and fasta file.\n"))
             sys.exit(-1)
         
     if config["datadir"]:
@@ -148,10 +158,17 @@ def data_group_parsing(datadir,background_csv,background_fasta,data_column,confi
     # see if you can read it, 
     # check if data_column in csv header, 
     # count the number of records
-    csv_record_count = check_background_csv(config["background_csv"],config["data_column"])
-    fasta_record_count = check_background_fasta(config["background_fasta"])
-    
-    if csv_record_count != fasta_record_count:
-        sys.stderr.write(colour.cyan(f"Error: different number of background csv and background fasta records.\n")+"Please provide a fasta record for each row in the background metadata file.\n")
-        sys.exit(-1)
 
+    if not debug:
+        csv_record_count = check_csv_file("-bc/--background-csv","background csv",config["background_csv"],config["data_column"])
+        fasta_record_count = check_background_fasta(config["background_fasta"])
+        
+        if csv_record_count != fasta_record_count:
+            sys.stderr.write(cyan(f"Error: different number of background csv and background fasta records.\n")+"Please provide a fasta record for each row in the background metadata file.\n")
+            sys.exit(-1)
+
+        if config["background_SNPs"]:
+            SNP_record_count = check_csv_file("-bSNP/--background-SNPs","background SNPs",config["background_SNPs"],config["data_column"])
+            if csv_record_count != SNP_record_count:
+                sys.stderr.write(cyan(f"Error: different number of background csv and background SNP records.\n")+"Please provide a SNP record for each row in the background metadata file.\n")
+                sys.exit(-1)
