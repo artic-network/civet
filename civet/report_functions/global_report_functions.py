7d52c2cd3cf9137b07678fb62bf69d308579a4ad
ViralVerity <verity.hill@ed.ac.uk>
2021-06-16 16:20:55 +0100

    move date and name functions to new global report functions script

--- civet/report_functions/global_report_functions.py
+++ civet/report_functions/global_report_functions.py
@@ -1,10 +1,9 @@
+
 import csv
 import sys
 import os
-import random
 from civet.utils import misc
-from civet.utils.log_colours import green,cyan
-
+from civet.utils.log_colours import green,cyan,red
 
 def sequence_name_parsing(report_column, anonymise, config):
     """
@@ -88,3 +87,46 @@ def write_names_to_file(config, name_dict):
     misc.add_col_to_metadata("display_name",name_dict, config["query_metadata"], new_metadata, config["input_column"], config)
 
     config["query_metadata"] = new_metadata
+
+def qc_date_col(column_arg, config, metadata, metadata_name, cl_arg):
+
+    with open(metadata) as f:
+        reader = csv.DictReader(f)
+    
+        if config[column_arg]:
+            if config[column_arg] not in reader.fieldnames:
+                sys.stderr.write(cyan(f"Error: {column_arg} column not found in {metadata_name} metadata file. Please specifiy which column to match with {cl_arg}`\n"))
+                sys.exit(-1)
+        else:
+            if "sample_date" in reader.fieldnames:
+                config[column_arg] = "sample_date"
+
+    if config[column_arg]:
+        count = 0
+        with open(metadata) as f:
+            data = csv.DictReader(f)
+            for line in data:
+                count += 1
+                if line[config[column_arg]] != "":
+                    misc.check_date_format(line[config[column_arg]], count, config[column_arg])    
+
+def parse_date_args(date_column, background_date_column, config):
+
+    """
+    parses the report group arguments:
+    --date-column (default: sample_date if present, False if not)
+    --background-date-column (default: sample_date if present, False if not)
+    """
+
+    misc.add_arg_to_config("date_column", date_column, config)
+    misc.add_arg_to_config("background_date_column", background_date_column, config)
+
+    if "input_csv" in config:
+        qc_date_col("date_column", config, config["input_csv"], "input", "-date/--date-column")
+    elif config["date_column"]:
+        sys.stderr.write(cyan("You have provided a column header to find date in the input csv using '-date/--date-column' but no input csv. Please use '-bdate/--background-date-column to provide date information from the background metadata, or provide an input csv with this data\n"))
+        sys.exit(-1)
+
+    qc_date_col("background_date_column", config, config["background_csv"], "background", "-bdate/--background-date-column")
+
+
--- civet/report_functions/table_functions.py
+++ civet/report_functions/table_functions.py
@@ -62,44 +62,5 @@ def sort_default_headers(input_fieldnames, background_fieldnames, config):
 
     config["table_content"] = header_list
 
-def qc_date_col(column_arg, config, metadata, metadata_name, cl_arg):
 
-    with open(metadata) as f:
-        reader = csv.DictReader(f)
-    
-        if config[column_arg]:
-            if config[column_arg] not in reader.fieldnames:
-                sys.stderr.write(cyan(f"Error: {column_arg} column not found in {metadata_name} metadata file. Please specifiy which column to match with {cl_arg}`\n"))
-                sys.exit(-1)
-        else:
-            if "sample_date" in reader.fieldnames:
-                config[column_arg] = "sample_date"
-
-    if config[column_arg]:
-        count = 0
-        with open(metadata) as f:
-            data = csv.DictReader(f)
-            for line in data:
-                count += 1
-                if line[config[column_arg]] != "":
-                    misc.check_date_format(line[config[column_arg]], count, config[column_arg])    
-
-def parse_date_args(date_column, background_date_column, config):
-
-    """
-    parses the report group arguments:
-    --date-column (default: sample_date if present, False if not)
-    --background-date-column (default: sample_date if present, False if not)
-    """
-
-    misc.add_arg_to_config("date_column", date_column, config)
-    misc.add_arg_to_config("background_date_column", background_date_column, config)
-
-    if "input_csv" in config:
-        qc_date_col("date_column", config, config["input_csv"], "input", "-date/--date-column")
-    elif config["date_column"]:
-        sys.stderr.write(cyan("You have provided a column header to find date in the input csv using '-date/--date-column' but no input csv. Please use '-bdate/--background-date-column to provide date information from the background metadata, or provide an input csv with this data\n"))
-        sys.exit(-1)
-
-    qc_date_col("background_date_column", config, config["background_csv"], "background", "-bdate/--background-date-column")
  
