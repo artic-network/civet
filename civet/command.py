f8854f5d3214c48fb2fc25f56d06dcf4450d6142
aineniamh <aine.otoole@ed.ac.uk>
2021-06-18 13:46:17 +0100

    location col

--- civet/command.py
+++ civet/command.py
@@ -154,7 +154,7 @@ def main(sysargs = sys.argv[1:]):
 
     # Define what's going to go in the report and sort global report options 
     # stored under config = { "report_content": [1, 2, 3, 4], "reports": [1,2,3,4],[1,2]}
-    name_dict = report_arg_parsing.report_group_parsing(args.report_content,args.report_column, args.anonymise, args.date_column, args.background_date_column,args.map_file,args.location, args.table_content, args.timeline_dates, args.timeline_colours, args.background_map_date_restriction, args.background_map_location, args.longitude_column, args.latitude_column, found_in_background_data, config)
+    name_dict = report_arg_parsing.report_group_parsing(args.report_content,args.report_column, args.anonymise, args.date_column, args.background_date_column,args.map_file,args.location_column, args.table_content, args.timeline_dates, args.timeline_colours, args.background_map_date_restriction, args.background_map_location, args.longitude_column, args.latitude_column, found_in_background_data, config)
 
     # sets up the output dir, temp dir, and data output desination
     directory_setup.output_group_parsing(args.outdir, args.output_prefix, args.overwrite,args.datestamp, args.output_data, args.tempdir, args.no_temp, config)
