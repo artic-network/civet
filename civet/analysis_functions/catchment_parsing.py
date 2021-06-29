18528b14f7b3ce5df51ef357ad5221356a63c611
aineniamh <aine.otoole@ed.ac.uk>
2021-06-29 17:51:20 +0100

    handling csv field limit

--- civet/analysis_functions/catchment_parsing.py
+++ civet/analysis_functions/catchment_parsing.py
@@ -5,9 +5,8 @@ import hashlib
 from numpy.random import choice
 import sys
 from civet.utils.log_colours import green,cyan,red
-from datetime import datetime
 import os
-import yaml
+csv.field_size_limit(sys.maxsize)
 
 def add_to_hash(seq_file,seq_map,hash_map,records):
     for record in SeqIO.parse(seq_file, "fasta"):
@@ -262,21 +261,3 @@ def downsample_if_building_trees(in_csv, out_csv, config):
                     for new_row in downsample_metadata:
                         writer.writerow(new_row)
 
-def test_downsampling(configfile):
-    print(datetime.now())
-    with open(configfile, 'r') as f:
-        config_loaded = yaml.safe_load(f)
-
-    fasta  = os.path.join(config_loaded["tempdir"],"hashed.aln.fasta")
-    in_csv = os.path.join(config_loaded["data_outdir"],"catchments","query_metadata.catchments.csv")
-    catchment_dir = os.path.join(config_loaded["data_outdir"],"catchments")
-    out_csv = os.path.join(config_loaded["data_outdir"],"catchments","master_metadata.downsample.csv")
-    print(datetime.now())
-    downsample_if_building_trees(in_csv, out_csv, config_loaded)
-    print(datetime.now())
-    if '3' in config_loaded["report_content"]:
-        print(green("Writing catchment fasta files."))
-        write_catchment_fasta(out_csv,fasta,catchment_dir,config_loaded)
-        print(datetime.now())
-
-test_downsampling("/Users/s1680070/repositories/civet3/civet_2021-06-29/catchments/config.yaml")
