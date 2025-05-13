8c55827ec3976c01d3b6d83cb5e1860050c27d95
aineniamh <aine.otoole@ed.ac.uk>
2025-05-13 16:17:50 +0100

    adding red def

--- civet/utils/misc.py
+++ civet/utils/misc.py
@@ -4,8 +4,12 @@ import csv
 import sys
 import datetime as dt
 
-from civet.utils.log_colours import red,green,cyan
-from civet.utils.config import *
+import snakemake
+
+
+import piranha.utils.custom_logger as custom_logger
+from piranha.utils.log_colours import green,cyan,red
+from piranha.utils.config import *
 
 def run_snakemake(snake_config,snakefile,config):
     if config[KEY_VERBOSE]:
